5d0d2ff7104558c5ddfd4818bac353040b34357d
Jeremy List <quick.dudley@gmail.com>
2017-07-01 23:21:48 +1200

    Hello World (but no way to run it yet)

---
+++ snippets/helloworld.cr
@@ -0,0 +1,4 @@
+-- Neither the compiler nor the interpreter are ready yet, but Hello World is
+-- the same in Carry and Haskell, so we can run this if we really want to.
+
+main = putStrLn "Hello World!"
