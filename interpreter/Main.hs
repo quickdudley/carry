b5f1877cd6d7105a55746b411c5ac0c70e71321c
Jeremy List <quick.dudley@gmail.com>
2020-05-19 20:14:05 +1200

    Slightly better `main` function: just shove the source files at the parser.

--- interpreter/Main.hs
+++ interpreter/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
+import Language.Carry.M
+import Language.Carry.Parser
+
+import Codec.Phaser
+import Data.Foldable
+import System.Environment
+
 main :: IO ()
-main = error "Too early in development"
+main = do
+  args <- getArgs
+  traverse_ (\filename -> parseFile sourceFile filename >>= \case
+     Right _ -> putStrLn $ "Parser reports success for `" ++ filename ++ "`"
+     Left fe -> do
+       putStrLn "Parse error:"
+       traverse_ (\ ~(p,pe) -> do
+         putStrLn $ show p ++ ":"
+         traverse_ (\e -> putStrLn $ "  " ++ e) pe
+        ) fe
+    ) args
